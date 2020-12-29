62bd893fef1af53a6928ac7109e4fcdaeaaa83af
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-12-29 18:25:09 -0300

    feat(backend): adicionar documentação da API

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log('OIEE');
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
+const app = express();
+
+const openApiDocs = YAML.load(
+    path.resolve(__dirname, './wwwroot/docs/openapi.yaml')
+);
+app.use('/docs/swagger', swaggerUi.serve, swaggerUi.setup(openApiDocs));
+
+app.listen(3030);
