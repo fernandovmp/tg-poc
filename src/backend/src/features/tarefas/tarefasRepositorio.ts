dbf6a12d7d4db37210c5fb1c936c28186a9de78b
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-12-30 15:34:26 -0300

    feat(backend): adicionar repositório de tarefas

---
+++ src/backend/src/features/tarefas/tarefasRepositorio.ts
@@ -0,0 +1,20 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import { EntidadeTarefa } from '../../entities/tarefa';
+import { ITarefaDto } from '@todo-list/shared';
+import { Service } from 'typedi';
+
+@Service()
+@EntityRepository(EntidadeTarefa)
+export class TarefasRepositorio extends Repository<ITarefaDto> {
+    public getAll() {
+        return this.createQueryBuilder()
+            .select('t')
+            .from(EntidadeTarefa, 't')
+            .getMany();
+    }
+
+    public async add(tarefa: ITarefaDto) {
+        await this.insert(tarefa);
+    }
+}
