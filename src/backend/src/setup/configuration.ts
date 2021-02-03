c6db92a65eb86a746522619fd9457d215c9170b6
Fernando Mariano Pereira <fernandovmp@outlook.com>
2021-02-03 19:24:00 -0300

    refact(backend): usar variáveis de ambiente para configurar o TypeORM

--- src/backend/src/setup/configuration.ts
+++ src/backend/src/setup/configuration.ts
@@ -5,10 +5,4 @@ const config = process.env;
 
 export default {
     port: config.PORT ?? 3030,
-    ormType: config.ORM_TYPE,
-    ormHost: config.ORM_HOST,
-    ormPort: config.ORM_PORT,
-    ormUsername: config.ORM_USERNAME,
-    ormPassword: config.ORM_PASSWORD,
-    ormDatabase: config.ORM_DATABASE,
 };
--- src/backend/src/setup/useTypeOrm.ts
+++ src/backend/src/setup/useTypeOrm.ts
@@ -1,20 +1,8 @@
 import { createConnection, useContainer } from 'typeorm';
 
 import { Container } from 'typedi';
-import { EntidadeTarefa } from '../entities/tarefa';
-import configuration from './configuration';
 
 export function useTypeOrm() {
     useContainer(Container);
-    return createConnection({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres1234',
-        database: 'TodoList',
-        entities: [EntidadeTarefa],
-        synchronize: false,
-        logging: false,
-    });
+    return createConnection();
 }
